b871108c56baac9f9ba074a785b9226f9542ad3a
Grorruss <39708974+Grorruss@users.noreply.github.com>
2020-11-12 18:29:12 +0100

    Centre la souris sur le nez
    Modification afin que la souris soit bien centrée sur le nez

--- src/Demo.java
+++ src/Demo.java
@@ -1,9 +1,12 @@
+import java.awt.*;
 import java.io.File;
 import java.net.URL;
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacpp.*;
 import org.bytedeco.javacpp.indexer.*;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_core.Point;
 import org.bytedeco.opencv.opencv_imgproc.*;
 import org.bytedeco.opencv.opencv_calib3d.*;
 import org.bytedeco.opencv.opencv_objdetect.*;
@@ -56,36 +59,16 @@ public class Demo {
         // by the garbage collector, but may still be explicitly released by calling deallocate().
         // You shall NOT call cvReleaseImage(), cvReleaseMemStorage(), etc. on objects allocated this way.
         Mat grayImage = new Mat(height, width, CV_8UC1);
-        Mat rotatedImage = grabbedImage.clone();
-
-        // The OpenCVFrameRecorder class simply uses the VideoWriter of opencv_videoio,
-        // but FFmpegFrameRecorder also exists as a more versatile alternative.
-        FrameRecorder recorder = FrameRecorder.createDefault("output.avi", width, height);
-        recorder.start();
 
         // CanvasFrame is a JFrame containing a Canvas component, which is hardware accelerated.
         // It can also switch into full-screen mode when called with a screenNumber.
         // We should also specify the relative monitor/camera response for proper gamma correction.
         CanvasFrame frame = new CanvasFrame("Some Title", CanvasFrame.getDefaultGamma()/grabber.getGamma());
 
-        // Let's create some random 3D rotation...
-        Mat randomR    = new Mat(3, 3, CV_64FC1),
-                randomAxis = new Mat(3, 1, CV_64FC1);
-        // We can easily and efficiently access the elements of matrices and images
-        // through an Indexer object with the set of get() and put() methods.
-        DoubleIndexer Ridx = randomR.createIndexer(),
-                axisIdx = randomAxis.createIndexer();
-        axisIdx.put(0, (Math.random() - 0.5) / 4,
-                (Math.random() - 0.5) / 4,
-                (Math.random() - 0.5) / 4);
-        Rodrigues(randomAxis, randomR);
-        double f = (width + height) / 2.0;  Ridx.put(0, 2, Ridx.get(0, 2) * f);
-        Ridx.put(1, 2, Ridx.get(1, 2) * f);
-        Ridx.put(2, 0, Ridx.get(2, 0) / f); Ridx.put(2, 1, Ridx.get(2, 1) / f);
-        System.out.println(Ridx);
-
         // We can allocate native arrays using constructors taking an integer as argument.
         Point hatPoints = new Point(3);
+        Point nosePoints = new Point(1);
+        Robot myRobot = new Robot();
 
         while (frame.isVisible() && (grabbedImage = converter.convert(grabber.grab())) != null) {
             // Let's try to detect some faces! but we need a grayscale image...
@@ -97,6 +80,13 @@ public class Demo {
                 Rect r = faces.get(i);
                 int x = r.x(), y = r.y(), w = r.width(), h = r.height();
                 rectangle(grabbedImage, new Point(x, y), new Point(x + w, y + h), Scalar.RED, 1, CV_AA, 0);
+                nosePoints.position(0).x(x + w /2).y(y + h / 2);
+                circle(grabbedImage, nosePoints.position(0), w/10, Scalar.RED, CV_FILLED, CV_AA, 0);
+                Point reference = new Point(x + w/2, y + h/2);
+                Point offset = new Point(frame.getLocation().x, frame.getLocation().y);
+                myRobot.mouseMove(reference.x() + offset.x(), reference.y() + offset.y());
+
+
 
                 // To access or pass as argument the elements of a native array, call position() before.
                 hatPoints.position(0).x(x - w / 10     ).y(y - h / 10);
@@ -108,25 +98,10 @@ public class Demo {
             // Let's find some contours! but first some thresholding...
             threshold(grayImage, grayImage, 64, 255, CV_THRESH_BINARY);
 
-            // To check if an output argument is null we may call either isNull() or equals(null).
-            MatVector contours = new MatVector();
-            findContours(grayImage, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-            long n = contours.size();
-            for (long i = 0; i < n; i++) {
-                Mat contour = contours.get(i);
-                Mat points = new Mat();
-                approxPolyDP(contour, points, arcLength(contour, true) * 0.02, true);
-                drawContours(grabbedImage, new MatVector(points), -1, Scalar.BLUE);
-            }
-
-            warpPerspective(grabbedImage, rotatedImage, randomR, rotatedImage.size());
-
-            Frame rotatedFrame = converter.convert(rotatedImage);
+            Frame rotatedFrame = converter.convert(grabbedImage);
             frame.showImage(rotatedFrame);
-            recorder.record(rotatedFrame);
         }
         frame.dispose();
-        recorder.stop();
         grabber.stop();
     }
 }
